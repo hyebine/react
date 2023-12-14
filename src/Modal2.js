2d5e6893341ef428de0f909357e7960565c9e43f
hyebine <berryme015@gmail.com>
2023-12-14 13:07:15 +0900

    modal pop up

---
+++ src/Modal2.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function StaticExample() {
+  const [ ispop, setispop ] = useState(true);
+  return (
+    <>
+    {ispop && <div
+      className="modal show"
+      style={{ display: "block" , position: 'initial' }}
+    >
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>{ispop}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => { setispop(false) }}>Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>}
+
+    </>
+  );
+}
+
+export default StaticExample;
