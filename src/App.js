59f9efc9de72e5956886cfa0696d9ae802aafb1a
hyeibine <berryme015@gmail.com>
2023-12-18 13:04:47 +0900

    navi router 적용

--- src/App.js
+++ src/App.js
@@ -3,33 +3,53 @@ import navijson from './data/gnb.json';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Container, Navbar } from 'react-bootstrap';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   const [tabvar, settab] = useState(0);
-  const myfun = (v) => {
-    alert(v)
-  }
 
   return (
-    <div>
-      <ul className='d-flex'>
-        {
-          navijson.map((v, idx) => {
-            return (
-              <li onClick={function () { settab(idx); }}>{v.hbname}</li>
-            )
-          })
-        }
-      </ul>
-      <div className='border py-5 bg-dark text-white'>
-        {
-          navijson[tabvar] && <p>
-            <img src={navijson[tabvar].src } alt={navijson[tabvar].alt} />
-            <strong>{navijson[tabvar].hbname}</strong>
-          </p>
-        }
+
+    <div className='wrap'>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Link to="/kr">한국어</Link>
+          <Link to="/en">영어</Link>
+
+          <div className='text-white'>
+          <Routes>
+            <Route path='/kr' element={<>관리자</>}></Route>
+            <Route path='/en' element={<>admin</>}></Route>
+          </Routes>
+          </div>
+
+        </Container>
+      </Navbar>
+
+      <div id='tab'>
+        <ul className='d-flex'>
+          {
+            navijson.map((v, idx) => {
+              return (
+                <li onClick={function () { settab(idx); }}>{v.hbname}</li>
+              )
+            })
+          }
+        </ul>
+        <div className='border py-5 bg-dark text-white'>
+          {
+            navijson[tabvar] && <p>
+              <img src={navijson[tabvar].src} alt={navijson[tabvar].alt} />
+              <strong>{navijson[tabvar].hbname}</strong>
+            </p>
+          }
+        </div>
       </div>
+
     </div>
+
   );
 }
 
