84c1f86601abe929a2fc9978528f0f5be77fd056
hyebine <berryme015@gmail.com>
2024-01-03 13:09:30 +0900

    스타일 컴포넌트 예시

--- src/Btns.js
+++ src/Btns.js
@@ -1,15 +1,30 @@
-import {Wbtn, Bbtn } from './styled/Commonui'
+import { Wbtn, Bbtn, BgBanner, WildBanner, Parentwbtn } from './styled/Commonui'
+import styled from 'styled-components'
+
+
+const Outcom = styled(BgBanner)`
+background-color: red;
+color: white;
+font-size: 50px;
+`
 
 const Btns = () => {
-  
+
   const bodybg = {
-    backgroundColor : "black",
-    padding : "5rem",
-    textAlign : "center"
+    backgroundColor: "black",
+    padding: "5rem",
+    textAlign: "center"
   }
 
   return (
     <div style={bodybg}>
+      <BgBanner bgcolor="gray">
+        <h2>큰글씨</h2>
+        <p>작은글씨</p>
+      </BgBanner>
+      <Outcom>평범한 컴포넌트를 스타일 컴포넌트가 가져옴</Outcom>
+      <WildBanner>스타일컴포넌트그대로</WildBanner>
+      <Parentwbtn>내안에 Wbtn 클래스가 있다.</Parentwbtn>
       <Wbtn>하얀버튼</Wbtn>
       <Bbtn>검정버튼</Bbtn>
     </div>
--- src/styled/Commonui.js
+++ src/styled/Commonui.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+
 export const Wbtn = styled.button`
   background-color: black;
   color: white;
@@ -9,7 +10,6 @@ export const Wbtn = styled.button`
   text-align: center;
   width: 100px;
   line-height: 2rem;
-
 `;
 
 
@@ -23,3 +23,29 @@ export const Bbtn = styled.button`
   width: 100px;
   line-height: 2rem;
 `;
+
+/*
+스타일컴포넌트 생성
+  1. 이름은 Wildbanner
+  2. 태그는 div로 출력
+  3. 배경색이 검정, 글자 하얀색, 가운데 정렬, 위아래 여백은 200
+*/
+export const WildBanner = styled.div`
+background-color: black;
+color: white;
+text-align: center;
+padding: 200px 0;
+`;
+
+export const Parentwbtn = styled(Wbtn)`
+border: 10px solid green;
+`;
+
+
+export const BgBanner = (props) => {
+  return (
+    <div className={props.className} style={{backgroundColor : props.bgcolor}}>
+      {props.children}
+    </div>
+  )
+}
